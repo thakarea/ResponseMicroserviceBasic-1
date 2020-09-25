e91ad4cbacaf4115f985d2a6ad3d1b81df03d4c7
thakarea <akash.thakarea@gmail.com>
2020-09-25 21:20:34 +0530

    Request App -1 commit

---
+++ src/main/java/com/bse/ResponseSpringBootApp/ResponseSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.bse.ResponseSpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponseSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResponseSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bse/ResponseSpringBootApp/controller/ResponseAppController.java
@@ -0,0 +1,23 @@
+package com.bse.ResponseSpringBootApp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bse.ResponseSpringBootApp.dto.EmployeeDto;
+import com.bse.ResponseSpringBootApp.service.ResponseAppService;
+
+@RestController
+public class ResponseAppController {
+
+	@Autowired
+	private ResponseAppService responseAppService;
+	
+	@GetMapping("emplDetail/bse")
+	public EmployeeDto getEmpDetails(@RequestParam("payGroup") String paygroup)
+	{
+		return responseAppService.fetchEmp(paygroup);
+	}
+	
+}
